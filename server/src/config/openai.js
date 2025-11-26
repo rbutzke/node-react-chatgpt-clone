471eb1957071dba78eb9e9650c5135d7e4ea7abc
Roberto Butzke Junior <45541285+rbutzke@users.noreply.github.com>
2025-11-26 14:40:26 -0300

    Update openai.js

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -16,7 +16,7 @@ module.exports = class openai{
 			model:"text-davinci-003",
 			prompt:`${prompt}`,
 			temperature:0,
-			max_tokens: 3500,
+			max_tokens: 256,
 			top_p:1,
 			frequency_penalty: 0.5,
 			presence_penalty: 0
